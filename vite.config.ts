33f4b7ee0cc18bc8fd06174ac31b336ec72ae235
iancleary <github@iancleary.me>
2024-10-30 11:18:49 -0700

    Update to regular netlify functions, not edge

--- vite.config.ts
+++ vite.config.ts
@@ -1,7 +1,7 @@
 import { vitePlugin as remix } from "@remix-run/dev";
 import { defineConfig } from "vite";
 import tsconfigPaths from "vite-tsconfig-paths";
-import { netlifyPlugin } from "@netlify/remix-edge-adapter/plugin";
+import { netlifyPlugin } from "@netlify/remix-adapter/plugin";
 import styleX from "vite-plugin-stylex";
 
 declare module "@remix-run/node" {
